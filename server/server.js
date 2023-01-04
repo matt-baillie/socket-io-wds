42ba951d37ec4519a0295d50412fa436793f81d5
Matthew Baillie <mbaillie93@gmail.com>
2023-01-04 00:28:38 -0800

    Socket connection established. IP or localhost must be explicitly written depending on which address is in browser

--- server/server.js
+++ server/server.js
@@ -1,4 +1,9 @@
-const io = require("socket.io")(3000);
+const io = require("socket.io")(3000, {
+  cors: {
+    origin: "http://127.0.0.1:5500",
+    methods: ["GET", "POST"],
+  },
+});
 
 io.on("connection", (socket) => {
   console.log(socket.id);
